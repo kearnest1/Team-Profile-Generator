b49ee4a350013bed2c503304d796ee9dfbc47143
Kristin Earnest <kearnest10@gmail.com>
2020-08-15 01:31:17 -0500

    files and folders

---
+++ app.js
@@ -0,0 +1,31 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manger = require("../lib/Manger");
+
+const outputPath = path.resolve(__dirname, "output", "index.html")
+
+test  ('creates an app for employees', () => {
+    const employee = new Employee('Kristin');
+  
+    expect(employee.name).toBe('Kristin');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('Kearnest10@gmail.com');
+  });
+
+
+  test('gets information from employee or returns false', () => {
+    const employee = new Employee('Kristin');
+  
+    expect(employee.getInformation()).toEqual(expect.any(Array));
+  
+    employee.information = [];
+  
+    expect(employee.getInformation()).toEqual(false);
+  });
+  
+test("gets employee's id value", () => {
+  const employee = new Employee ('Kristin');
+
+  expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
